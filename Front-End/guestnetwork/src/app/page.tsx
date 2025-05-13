72081608a5b6b59348d365a61fa70af0f845e9a0
Stef1828 <91679251+Stef1828@users.noreply.github.com>
2025-05-13 10:25:37 +0200

    QR-code component

--- Front-End/guestnetwork/src/app/page.tsx
+++ Front-End/guestnetwork/src/app/page.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
+import {QRCodeSVG} from 'qrcode.react';
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Image
-        src="/images/guestnetwork.png"
-        alt="Guest Network Logo"
+        src="/Images/UCLL-logo.jpg"
+        alt="UCLL logo"
         width={500}
         height={500}
-      />
+        className="rounded-lg"
+        priority
+      ></Image>
+      <QRCodeSVG value="https://reactjs.org/"/>
     </div>
   );
 }
