10fa7380c8d7a54c66553471a722488f0b4eae50
Vincent <vincent.degreef@student.ucll.be>
2025-05-15 12:16:59 +0200

    better service

--- Front-End/guestnetwork/src/app/Services/PasswordService.ts
+++ Front-End/guestnetwork/src/app/Services/PasswordService.ts
@@ -1,5 +1,8 @@
+const basicUrl = `${process.env.NEXT_PUBLIC_API_URL}`;
+
+
 const getPassword = async () => {
-    return fetch(`${process.env.NEXT_PUBLIC_API_URL}/password`, {
+    return fetch(`${basicUrl}/password`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'          
