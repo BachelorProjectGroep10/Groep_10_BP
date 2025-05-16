a6586173c38e1539eada1aad5cdfd1ee83b4ee16
Vincent <vincent.degreef@student.ucll.be>
2025-05-16 15:51:08 +0200

    logout fix

--- Front-End/guestnetwork/src/app/dashboard/page.tsx
+++ Front-End/guestnetwork/src/app/dashboard/page.tsx
@@ -1,9 +1,21 @@
+'use client'
 import Background from "@/app/Components/background";
 import { HeaderComponent } from "@/app/Components/header";
 import QRCodeComponent from "../Components/qrCode";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 
 export default function Dashboard() {
+  const router = useRouter();
+  useEffect(() => {
+    const admin = sessionStorage.getItem("admin");
+    if (!admin) {
+      setTimeout(() => {
+        router.push("/");
+      }, 1000);
+    }
+  }, []);
   return (
       <div className="relative w-screen overflow-x-hidden overflow-y-auto md:h-screen md:overflow-hidden">
         <div className="absolute top-0 left-0 w-screen h-screen">
