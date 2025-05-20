56349de7592fea323f439871a7366ed1843d8e2f
Vincent <vincent.degreef@student.ucll.be>
2025-05-20 14:46:11 +0200

    refactored overview

---
+++ Front-End/guestnetwork/src/app/Components/Overview/Cards/userCard.tsx
@@ -0,0 +1,41 @@
+import { User } from "@/app/Types";
+import formatDate from "../../formatDate";
+import { useTranslation } from "react-i18next";
+
+interface UserCardProps {
+  user: User
+}
+
+export default function UserCard({ user }: UserCardProps) {
+  const { t } = useTranslation();
+
+  return (
+    <div className="w-full max-w-md mx-auto bg-white text-left shadow-lg rounded-xl p-4 space-y-2">
+      <div className="text-md font-semibold text-[#003366]">{user.macAddress}</div>
+      <div className="text-sm">
+        <strong className="text-black">Password:</strong> {user.password}
+      </div>
+      <div className="text-sm text-[#003366] space-y-1">
+        <p>{user.email}</p>
+        <p className="font-semibold">{user.studentNumber}</p>
+      </div>
+
+      <div className="text-sm text-[#003366]">
+        <p><strong>User ID:</strong> {user.id}</p>
+        <p><strong>Group ID:</strong> {user.groupId}</p>
+      </div>
+
+      <p className="text-sm text-[#003366]">
+        <strong>Expires At:</strong> {formatDate(user?.expiredAt)}
+      </p>
+
+      <span
+        className={`inline-block px-4 py-1 rounded-full text-xs font-bold ${
+          user.active ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"
+        }`}
+      >
+        {user.active ? t("overview.yes") : t("overview.no")}
+      </span>
+    </div>
+  );
+}
---
+++ Front-End/guestnetwork/src/app/Components/Overview/Tables/groupsTable.tsx
@@ -0,0 +1,45 @@
+import { Group } from "@/app/Types";
+import formatDate from "../../formatDate";
+import { useTranslation } from "react-i18next";
+import '../../../i18n';
+
+interface GroupsTableProps {
+  groups: Group[]
+}
+
+export default function GroupsTable( { groups }: GroupsTableProps) {
+  const {t} = useTranslation();
+
+  return (
+    <div className="overflow-x-auto border-t border-gray-200 pt-4 bg-white rounded-lg">
+      <table className="min-w-full table-auto border-collapse ">
+        <thead>
+          <tr className=" text-[#003366] text-left">
+            <th className="px-6 py-3 font-semibold">ID</th>
+            <th className="px-6 py-3 font-semibold">{t('overview.groupName')}</th>
+            <th className="px-6 py-3 font-semibold">{t('overview.description')}</th>
+            <th className="px-6 py-3 font-semibold">{t('overview.password')}</th>
+          </tr>
+        </thead>
+      </table>
+      <div className="max-h-[225px] overflow-y-auto">
+        <table className="min-w-full table-auto border-collapse">
+          <tbody>
+            {groups.map((group: Group, idx: number) => (
+              <tr
+                key={group.id}
+                className="hover:bg-[#9FDAF9] text-[#003366] text-left border-b border-gray-200"
+              >
+                <td className="px-6 py-4">{group.id}</td>
+                <td className="px-6 py-4">{group.groupName}</td>
+                <td className="px-6 py-4">{group.description}</td>
+                <td className="px-6 py-4">{group.password}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+
+}
